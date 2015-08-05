10631247c8dea4e28feee0881af98634157329a5
mbrickenworthe <adamwinters456@gmail.com>
2015-08-05 19:33:35 -0400

    Creating my first Sinatra app

--- app.rb
+++ app.rb
@@ -1,5 +1,26 @@
 require './config/environment'
 
 class TestApp < Sinatra::Base
+  get '/about' do
+
+    erb :about
+
+  end
+
+  get '/hello' do
+
+    @name = (params[:name])
+
+    erb :hello
+  
+  end
+
+  post '/' do
+  end
+
+  get '/' do
+
+  end
+
   #put your route handlers here
 end
