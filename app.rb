19e1debc97e00e942eeb58eff2a57ced786d4014
garettarrowood <garettarrowood@gmail.com>
2015-08-20 13:38:24 -0400

    question mark placement

--- app.rb
+++ app.rb
@@ -1,5 +1,28 @@
 require './config/environment'
 
 class TestApp < Sinatra::Base
-  #put your route handlers here
+  
+  get '/' do
+  	
+  end
+
+  post '/' do
+
+  end
+
+  get '/about' do
+  	erb :about
+  end
+
+  get '/hello/?:name?' do
+  	@name = params[:name]
+  	erb :hello
+  end
+
+  get '/hello' do
+  end
+
 end
+
+
+
