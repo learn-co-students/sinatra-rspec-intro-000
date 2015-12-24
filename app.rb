a2e1f412a207d38c73cae8b2ebf107370b6b3e43
NoneOfMaster <rcraigt@gmail.com>
2015-12-23 16:45:54 -0800

    funny sinatra rspec pun

--- app.rb
+++ app.rb
@@ -1,5 +1,22 @@
 require './config/environment'
 
 class TestApp < Sinatra::Base
-  #put your route handlers here
+
+  get '/' do
+
+  end
+  
+  post '/' do
+
+  end
+
+  get '/about' do
+    erb :'/about'
+  end
+
+  get '/hello' do
+    @name = params['name']
+    erb :'/hello'
+  end
+
 end
